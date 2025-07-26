55469e0f6ea817497687f0fa0b1a6359b465ca0a
Alfonso Lara <orozcoenrique159@gmail.com>
2025-07-26 11:33:45 -0500

    ejercicios clase 1

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int likes = 120;
+        System.out.println("tu publicación tiene " + likes + " likes");
+
+        likes += 100;
+
+        System.out.println("tu publicación tiene " + likes + " likes");
+
+        // Ejercicio: Declara una variable llamada vistas igual a 500 y muestra el total
+    }
+}
