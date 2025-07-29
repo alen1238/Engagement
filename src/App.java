35adf2e050efe9fcb1c72f8daec7ca2b2f713aaf
Alfonso Lara <orozcoenrique159@gmail.com>
2025-07-28 19:19:20 -0500

    proyecto final agregado

--- src/App.java
+++ src/App.java
@@ -1,6 +1,42 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int likes = 120;
+       int likes = 100;
+       int comments = 45;
+       int shares = 20;
+       double rating = 7.5;
+       String contenido1 = "Pelicula Accion: Jhon wick";
+
+       int visualizaciones = 1000;
+       
+       double engagement = (likes + comments + shares + rating)* 100 / visualizaciones;
+       System.out.println("Engagement publicacion 1" + engagement);
+
+       int likes2 = 250;
+       int comments2 = 76;
+       int shares2 = 12;
+       double rating2 = 7.9;
+       String contenido2 = "Pelicula Ciencia Ficción: Volver Al futuro";
+
+       int visualizaciones2 = 1000;
+       
+       double engagement2 = (likes2 + comments2 + shares2 + rating2)* 100 / visualizaciones2;
+       System.out.println("Engagement publicacion 2" + engagement2);
+
+       if(engagement > engagement2){
+        System.out.println("Sugerencias:");
+        System.out.println(contenido1);
+        System.out.println(contenido2);
+       }
+       else{
+         System.out.println("Sugerencias:");
+         System.out.println( contenido2);
+         System.out.println( contenido1);
+       }
+
+    }
+
+    public static void parte1(){
+         int likes = 120;
         System.out.println("tu publicación tiene " + likes + " likes");
 
         likes += 100;
@@ -8,5 +44,14 @@ public class App {
         System.out.println("tu publicación tiene " + likes + " likes");
 
         // Ejercicio: Declara una variable llamada vistas igual a 500 y muestra el total
+    
+        double rating = 7.5;
+
+        boolean viral = false;
+
+        String title = "Este es mi primer Vlog";
+
+
+    
     }
 }
